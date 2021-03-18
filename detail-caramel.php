485ab1c0f8ff1c41dacd9b946fcb50775e1b154a
Bhagas Nugroho <bhagas@desktopip.com>
2021-03-19 05:41:17 +0700

    fixing detail tiramisu

--- detail-caramel.php
+++ detail-caramel.php
@@ -21,35 +21,53 @@
   </div>
 </section>
 <section class="bg-yellow row no-gutters align-items-center justify-content-center panel">
-  <img id="caramel_hand" src="assets/images/Group 70.png" alt="">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-6"></div>
+  <div class="container-fluid">
+    <div class="row align-items-center">
       <div class="col-md-6">
-        <div class="detail-product-content">
-          <div class="detail-product-content__title">RASA YANG MANTUL</div>
-          <div class="detail-product-content__desc">
-            Rasa caramel yang brazza di lidah dengan kopi susu yang creamvy abiz, Neo Coffee Caramel Macchiato! Selalu
-            bikin hari-hari lo nggak biasa dan pastinya makin seru. Bisa lo nikmatin di mana aja dan kapan aja.
+        <div class="glass-container">
+          <img id="caramel_hand" class="" src="assets/images/Group 70.png" alt="">
+          <div class="shop-action">
+            <button>Get Your Neo Coffee <img src="assets/images/icon-chevron.png" alt=""></button>
+            <a href="#"><img src="assets/images/icon-shopee.png" alt="shopee"></a>
+            <a href="#"><img src="assets/images/icon-tokped.png" alt="shopee"></a>
           </div>
-          <br />
-          <br />
-          <br />
-          <div class="detail-product-content__title">INGREDIENTS</div>
-          <div class="detail-product-content__desc">
-            Gula, Krimer Nabati, Kopi Bubuk Instan, Perisa Sintetik Karamel, Pemanis Buatan Sukralosa dan Asesulfam-K.
-          </div>
-          <br />
-          <br />
-          <br />
-          <div class="detail-product-content__title">Servings: 1 (20g)</div>
-          <div class="detail-product-content__desc">
-            Tuang 1 sachet ke dalam gelas, tambahkan 150ml air panas, aduk dan siap dihidangkan.
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="row">
+          <div class="col-md-9">
+            <div class="detail-product-content">
+              <div class="detail-product-content__title">RASA YANG MANTUL</div>
+              <div class="detail-product-content__desc">
+                Rasa caramel yang brazza di lidah dengan kopi susu yang creamvy abiz, Neo Coffee Caramel Macchiato!
+                Selalu
+                bikin hari-hari lo nggak biasa dan pastinya makin seru. Bisa lo nikmatin di mana aja dan kapan aja.
+              </div>
+              <br />
+              <br />
+              <div class="detail-product-content__title">INGREDIENTS</div>
+              <div class="detail-product-content__desc">
+                Gula, Krimer Nabati, Kopi Bubuk Instan, Perisa Sintetik Karamel, Pemanis Buatan Sukralosa dan
+                Asesulfam-K.
+              </div>
+              <br />
+              <br />
+              <div class="detail-product-content__title">Servings: 1 (20g)</div>
+              <div class="detail-product-content__desc">
+                Tuang 1 sachet ke dalam gelas, tambahkan 150ml air panas, aduk dan siap dihidangkan.
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
+
+</section>
+<section class="panel bg-green">
+  <img class="w-100 mt-5" src="assets/images/caramel-people.png" alt="">
+</section>
+<section class="panel bg-green">
 </section>
 
 <?php include 'components/_commons-js.php'?>
