27dfb61f7d64d4cc9617232b5577cb1fc52c1454
Bhagas Nugroho <bhagas@desktopip.com>
2021-03-19 04:54:19 +0700

    product main page

---
+++ product.php
@@ -0,0 +1,33 @@
+<?php include 'components/_head.php'?>
+
+<section class="section-detail-headline panel bg-yellow">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-5 col-7">
+        <img class="w-100 mb-5 pb-md-4" src="assets/images/product-title.png" alt="">
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-md-4 col-6"><a href="detail-moccachino.php" class="sachet-link">
+          <img class="w-100 mb-md-0 mb-3" src="assets/images/headline-sachet-moccachino.png" alt="">
+        </a></div>
+      <div class="col-md-4 col-6"><a href="detail-caramel.php" class="sachet-link">
+          <img class="w-100 mb-md-0 mb-3" src="assets/images/headline-sachet-caramel.png" alt="">
+        </a></div>
+      <div class="col-md-4 col-6"><a href="detail-tiramisu.php" class="sachet-link">
+          <img class="w-100 mb-md-0 mb-3" src="assets/images/headline-sachet-tiramisu.png" alt="">
+        </a></div>
+    </div>
+  </div>
+</section>
+
+<?php include 'components/_commons-js.php'?>
+
+<script>
+// var scene = document.querySelector('.section-detail-headline-scene');
+// var parallax = new Parallax(scene);
+</script>
+
+
+
+<?php include 'components/_footer.php'?>
