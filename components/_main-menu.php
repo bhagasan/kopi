cb4cd784416af9a6e95695c8a6f96d4819578c70
Bhagas Nugroho <bhagas@desktopip.com>
2021-03-21 00:01:54 +0700

    fixing menu responsive

--- components/_main-menu.php
+++ components/_main-menu.php
@@ -1,13 +1,13 @@
 <div id="mainMenu" class="main-menu">
   <div class="row w-100">
-    <div class="col-md-7">
-      <div class="main-menu__text-container">
+    <div class="col-lg-7">
+      <div class="main-menu__text-container text-lg-right text-center">
         <span>M</span>
         <span>E</span>
         <span>N</span>
         <span>U</span>
       </div>
-      <div class="main-menu__img-container">
+      <div class="main-menu__img-container d-lg-block d-none">
         <!-- <img src="assets/images/img-on-main-menu.png" alt=""> -->
         <div class="main-menu-thumb" id="thumbHome">
           <img src="assets/images/menu_home_b.png" alt="">
@@ -28,7 +28,7 @@
         </div>
       </div>
     </div>
-    <div class="col-md-5">
+    <div class="col-lg-5 text-lg-left text-center">
       <ul>
         <li class="link-thumb" data-target="#thumbHome">
           <a href="#">HOME</a>
--- ini-mau-gue.php
+++ ini-mau-gue.php
@@ -6,9 +6,16 @@
       <div class="carousel-item active">
         <div class="slide-item-container" style="background-image:url(assets/images/ini-mau-gue-people-1.png)">
           <div class="container">
-            <div class="row align-item-center justify-content-end">
-              <div class="col-6">
+            <div class="row align-item-center pt-md-0 pt-5 justify-content-md-end justify-content-center">
+              <div class="col-lg-6 col-md-8 col-11 text-right">
                 <img class="ini-mau-gue-text mt-5" src="assets/images/ini-mau-gue-text-2.png" alt="">
+              </div>
+              <div class="col-10 text-md-right text-center">
+                <div class="ini-mau-gue-paragraph">
+                  Buat jalanin cara gue, kejar impian gue, dan dapetin mau gue,<br /><strong>butuh NEO Coffee yang tahu
+                    #iniMauGue</strong>
+                </div>
+                <button class="btn btn--outline mt-2">Find Your #IniMauGue</button>
               </div>
             </div>
           </div>
@@ -21,9 +28,16 @@
       <div class="carousel-item">
         <div class="slide-item-container" style="background-image:url(assets/images/ini-mau-gue-people-2.png)">
           <div class="container">
-            <div class="row align-item-center justify-content-center">
-              <div class="col-6">
+            <div class="row align-item-center pt-md-0 pt-5 justify-content-center">
+              <div class="col-lg-6 col-md-8 col-11 text-center">
                 <img class="ini-mau-gue-text mt-5" src="assets/images/ini-mau-gue-text-2.png" alt="">
+              </div>
+              <div class="col-10 text-center">
+                <div class="ini-mau-gue-paragraph">
+                  Buat jalanin cara gue, kejar impian gue, dan dapetin mau gue,<br /><strong>butuh NEO Coffee yang tahu
+                    #iniMauGue</strong>
+                </div>
+                <button class="btn btn--outline mt-2">Find Your #IniMauGue</button>
               </div>
             </div>
           </div>
@@ -36,9 +50,16 @@
       <div class="carousel-item">
         <div class="slide-item-container" style="background-image:url(assets/images/ini-mau-gue-people-3.png)">
           <div class="container">
-            <div class="row align-item-center">
-              <div class="col-6">
+            <div class="row align-item-center pt-md-0 pt-5">
+              <div class="col-lg-6 col-md-8 col-11 text-lg-right text-center">
                 <img class="ini-mau-gue-text mt-5" src="assets/images/ini-mau-gue-text-3.png" alt="">
+              </div>
+              <div class="col-12 text-center">
+                <div class="ini-mau-gue-paragraph">
+                  Buat jalanin cara gue, kejar impian gue, dan dapetin mau gue,<br /><strong>butuh NEO Coffee yang tahu
+                    #iniMauGue</strong>
+                </div>
+                <button class="btn btn--outline mt-2">Find Your #IniMauGue</button>
               </div>
             </div>
           </div>
@@ -51,9 +72,16 @@
       <div class="carousel-item">
         <div class="slide-item-container" style="background-image:url(assets/images/ini-mau-gue-people-4.png)">
           <div class="container">
-            <div class="row align-item-center justify-content-end">
-              <div class="col-6">
+            <div class="row align-item-center pt-md-0 pt-5 justify-content-lg-end justify-content-center">
+              <div class="col-lg-6 col-md-8 col-11 text-lg-right text-center">
                 <img class="ini-mau-gue-text mt-5" src="assets/images/ini-mau-gue-text-3.png" alt="">
+              </div>
+              <div class="col-12 text-center">
+                <div class="ini-mau-gue-paragraph">
+                  Buat jalanin cara gue, kejar impian gue, dan dapetin mau gue,<br /><strong>butuh NEO Coffee yang tahu
+                    #iniMauGue</strong>
+                </div>
+                <button class="btn btn--outline mt-2">Find Your #IniMauGue</button>
               </div>
             </div>
           </div>
@@ -66,9 +94,16 @@
       <div class="carousel-item">
         <div class="slide-item-container" style="background-image:url(assets/images/ini-mau-gue-people-5.png)">
           <div class="container">
-            <div class="row align-item-center">
-              <div class="col-6">
+            <div class="row align-item-center pt-md-0 pt-5 justify-content-lg-left justify-content-center">
+              <div class="col-lg-6 col-md-8 col-11">
                 <img class="ini-mau-gue-text mt-5" src="assets/images/ini-mau-gue-text-3.png" alt="">
+              </div>
+              <div class="col-12 text-center">
+                <div class="ini-mau-gue-paragraph">
+                  Buat jalanin cara gue, kejar impian gue, dan dapetin mau gue,<br /><strong>butuh NEO Coffee yang tahu
+                    #iniMauGue</strong>
+                </div>
+                <button class="btn btn--outline mt-2">Find Your #IniMauGue</button>
               </div>
             </div>
           </div>
@@ -81,7 +116,29 @@
     </div>
   </div>
 </section>
-<section class="panel bg-brown-2"></section>
+<section class="panel bg-yellow">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-7 text-center">
+        <!-- <div class="section-title">
+          <img src="" alt="">
+        </div> -->
+        <div class="ini-mau-gue-paragraph">
+          <strong>
+            Rasa kopi yang mantul dan varian yang sabi dari Neo Coffee<br />
+            bisa bantu gue dapetin #IniMauGue
+          </strong>
+        </div>
+        <button class="btn btn--fill mt-4 mb-5">Get Your Neo Coffee</button>
+      </div>
+    </div>
+    <div class="row no-gutters justify-content-center align-items-end neo-product">
+      <div class="col-lg-4 col-6"><img class="w-100" src="assets/images/neo-coffee-1.png" alt=""></div>
+      <div class="col-lg-4 col-6"><img class="w-100" src="assets/images/neo-coffee-2.png" alt=""></div>
+      <div class="col-lg-4 col-6"><img class="w-100" src="assets/images/neo-coffee-3.png" alt=""></div>
+    </div>
+  </div>
+</section>
 
 <?php include 'components/_commons-js.php'?>
 <script>
